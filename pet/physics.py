b2261b464cbc92d492c0264e6fb553ce5b87b8ad
caiobd <caiobd@protonmail.com>
2022-11-20 19:56:26 -0300

    black code formating

--- pet/physics.py
+++ pet/physics.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from dataclasses import dataclass
+
 
 @dataclass
 class RigidBody:
@@ -10,12 +12,12 @@ class RigidBody:
 
     def check_collision(self, other: RigidBody) -> bool:
         is_collision_in_x = (
-            other.x+other.width>=self.x>=other.x 
-            or other.x+other.width>=self.x+self.width>=other.x
+            other.x + other.width >= self.x >= other.x
+            or other.x + other.width >= self.x + self.width >= other.x
         )
         is_collision_in_y = (
-            other.y+other.height>=self.y>=other.y 
-            or other.y+other.height>=self.y+self.height>=other.y
+            other.y + other.height >= self.y >= other.y
+            or other.y + other.height >= self.y + self.height >= other.y
         )
         is_collision = is_collision_in_x and is_collision_in_y
         return is_collision
--- tests/test_pet.py
+++ tests/test_pet.py
@@ -2,4 +2,4 @@ from pet import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
