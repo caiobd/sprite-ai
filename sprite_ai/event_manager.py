10aa8875b8ce8eb1dbae0f69214aaeddf018f375
caio <caiobd@protonmail.com>
2023-11-26 02:00:47 -0300

    renamed variable in event manager

--- sprite_ai/event_manager.py
+++ sprite_ai/event_manager.py
@@ -10,7 +10,7 @@ Callback = Callable[[Any], None]
 class EventManager:
     def __init__(self) -> None:
         self.inscriptions: DefaultDict[str, list[Callback]] = defaultdict(list)
-        self.lock = Lock()
+        self._lock = Lock()
 
     def publish(self, topic: str, message: Any = None) -> None:
         """Publish message to a topic
@@ -20,7 +20,7 @@ class EventManager:
             topic (str): A topic to publish the object to
         """
         logger.debug(f"[{topic}] {message}")
-        with self.lock:
+        with self._lock:
             topic_callbacks = self.inscriptions[topic]
 
         for callback in topic_callbacks:
@@ -33,7 +33,7 @@ class EventManager:
             callback (Callback): a callable to be called when something is posted to the topic
             topic (str): the topic to link the callback to
         """
-        with self.lock:
+        with self._lock:
             self.inscriptions[topic].append(callback)
 
     def subscribers(self, topic: str) -> list:
