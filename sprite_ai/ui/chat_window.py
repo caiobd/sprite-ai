7e6ead473a649473e49240d993eb1a54b778478b
caio <caiobd@protonmail.com>
2024-01-14 21:09:26 -0300

    implement message list update with signals and slots

--- sprite_ai/ui/chat_window.py
+++ sprite_ai/ui/chat_window.py
@@ -1,5 +1,6 @@
 from time import time
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from sprite_ai.event_manager import EventManager
 from sprite_ai.ui.chat_window_ui import Ui_Form
 from sprite_ai.ui.message_delegate import MessageDelegate
@@ -10,6 +11,8 @@ USER_ME = 0
 USER_THEM = 1
 
 class ChatWindow(QtWidgets.QWidget):
+    ai_message_recived = pyqtSignal(dict)
+
     def __init__(self, event_manager: EventManager):
         super().__init__()
 
@@ -20,9 +23,11 @@ class ChatWindow(QtWidgets.QWidget):
         self.ui.setupUi(self)
         self.ui.lv_chat_history.setItemDelegate(MessageDelegate())
         self.ui.lv_chat_history.setModel(self.model)
+        self.ai_message_recived.connect(self.send_ai_message)
         self.ui.pb_send.clicked.connect(self.send_user_message)
-        self.event_manager.subscribe('ai_message', self.send_ai_message)
+        self.event_manager.subscribe('ai_message', self.ai_message_recived.emit)
     
+    @pyqtSlot()
     def send_user_message(self):
         user_message = self.ui.te_chatinput.toPlainText()
         self.model.add_message(USER_ME, user_message)
@@ -32,18 +37,8 @@ class ChatWindow(QtWidgets.QWidget):
         })
         self.ui.te_chatinput.clear()
 
+    @pyqtSlot(dict)
     def send_ai_message(self, message: dict):
         timestamp = message['timestamp']
         content = message['content']
         self.model.add_message(USER_THEM, content)
-
-
-
-# def main():
-#     app = QtWidgets.QApplication(sys.argv)
-#     application = ChatWindow()
-#     application.show()
-#     sys.exit(app.exec_())
-
-# if __name__ == "__main__":
-#     main()
