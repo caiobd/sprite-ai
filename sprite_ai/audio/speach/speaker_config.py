2207f3b40b15957757b870b7a44832c24698c1fc
caiobd <caiobd@protonmail.com>
2024-04-27 20:11:06 -0300

    added default voice for tts

--- sprite_ai/audio/speach/speaker_config.py
+++ sprite_ai/audio/speach/speaker_config.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+from importlib import resources
 from typing import Literal
 from pydantic import BaseModel
 
 
 class SpeakerConfig(BaseModel):
     language: str
-    voice_reference: str
+    voice_reference: str = str(
+        resources.path('sprite_ai.resources.voices', 'default.wav')
+    )
     inference_device: Literal['auto', 'cpu', 'cuda'] = 'auto'
