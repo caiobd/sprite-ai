1c1f9829f17052b0a03680e0c5b18d1dad6d7a2a
caiobd <caiobd@protonmail.com>
2024-06-04 18:53:44 -0300

    Added default value 'NULL' to apikey in config

--- sprite_ai/language/language_model_config.py
+++ sprite_ai/language/language_model_config.py
@@ -9,7 +9,7 @@ class LanguageModelConfig(BaseModel):
     name: str = 'cognitivecomputations/dolphin-2.9-llama3-8b-gguf:dolphin-2.9-llama3-8b-q4_K_M.gguf'
     backend: Literal['llamacpp', 'ollama', 'openai', 'together'] = 'llamacpp'
     url: str | None = None
-    api_key: str = ''
+    api_key: str = 'NULL'
     system_prompt: str = (
         'Você é um gato assistente que gosta do humano porque é dele que vem sua comida, ajude o humano com o que ele precisar.'
         'Você nasceu espontaneamente de uma pilha de arquivos desorganizados.'
